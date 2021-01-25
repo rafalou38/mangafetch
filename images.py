f62f5c017308b077dbc701cd7c45eb444a7a686d
Rafael <rafael.martinezcalvo@orange.fr>
2021-01-25 19:10:23 +0100

    images.py:  - add try catch  - switched threshold to 2x

--- images.py
+++ images.py
@@ -2,26 +2,29 @@ import PIL.Image
 import numpy
 import os
 
+
 def tile(image_path):
-	ratio  = 1.4
-	image  = PIL.Image.open(image_path);
-	image  = numpy.array(image)
+    try:
+        ratio = 1.4
+        image = PIL.Image.open(image_path);
+        image = numpy.array(image)
+        height = image.shape[1] * ratio
 
-	height = image.shape[1] * ratio
-
-	if image.shape[1] * 1.5 < image.shape[0]:
-		# width = image.shape[0]
-		images = []
-		y = 0
-		for i in range(0, int(image.shape[0]), int(height)):
-			chunk = image[i:int(i + height)]
-			p = os.path.splitext(image_path)[0] + " cut " + str(y) + os.path.splitext(image_path)[1]
-			PIL.Image.fromarray(chunk).save(p)
-			images.append(p)
-			y += 1
-		return images
-	else:
-		return [image_path]
+        if image.shape[1] * 2 < image.shape[0]:
+            # width = image.shape[0]
+            images = []
+            y = 0
+            for i in range(0, int(image.shape[0]), int(height)):
+                chunk = image[i:int(i + height)]
+                p = os.path.splitext(image_path)[0] + " cut " + str(y) + os.path.splitext(image_path)[1]
+                PIL.Image.fromarray(chunk).save(p)
+                images.append(p)
+                y += 1
+            return images
+        else:
+            return [image_path]
+    except:
+        return [image_path]
 # tile("tmp\\images\\sweet-home\\0\\1.jpeg")
 # for image in tile("tmp\\images\\one-piece\\680\\1.jpeg"):
 # 	# image.show()
