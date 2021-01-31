2d1e7488a23f6746253d68c46a20288c80a838ba
Rafael <rafael.martinezcalvo@orange.fr>
2021-01-31 20:00:50 +0100

    inverted order of sources and renamed to sources

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ from .scan_1_com import scan_1_com
 from .scansmangas_xyz import scansmangas_xyz
 from ._main import website
 
-apis = {
-    "scan_1_com": scan_1_com,
-    "scansmangas_xyz": scansmangas_xyz,
+sources = {
+    "scansmangas.xyz": scansmangas_xyz,
+    "scan-1.com": scan_1_com,
 }
