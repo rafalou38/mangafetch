5e28a069998a582965c86754a3a478fb3a4f8d47
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-05 16:38:57 +0100

    default download_chapter in website class

--- api/_main.py
+++ api/_main.py
@@ -3,6 +3,8 @@ import os
 from tempfile import TemporaryDirectory
 
 import requests
+from magic import magic
+
 from myLog import logger
 from urllib3.exceptions import ProtocolError
 
@@ -11,6 +13,8 @@ TMP_PATH = tmp_dir.name
 IMG_PATH = os.path.join(TMP_PATH, "images")
 OUT_PATH = "out/"
 PDF_PATH = os.path.join(TMP_PATH, "pdfs")
+
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
 
 session = requests.session()
 
@@ -52,3 +56,28 @@ class website:
     @classmethod
     def download_chapter(cls, chapter, manga_id, pages=None, img_url=None):
         pass
+
+    @classmethod
+    def download_chapter(cls, chapter, manga_id, pages=None):
+        init()
+        tries = 0
+        if pages is None:
+            pages = cls.get_pages(chapter, manga_id)
+        for page in pages:
+            try:
+                path = os.path.join(IMG_PATH, manga_id, str(chapter))
+                r = session.get(page, stream=True)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                buffer = r.iter_content(chunk_size=2048)
+                chunk = next(buffer)
+                mime = magic.from_buffer(chunk, mime=True)
+                extension = os.path.split(mime)[1]
+                filename = os.path.join(path, os.path.splitext(os.path.split(page)[1])[0] + "." + extension)
+                with open(filename, "wb") as f:
+                    f.write(chunk + r.raw.read())
+                yield filename
+            except Exception as err:
+                logger.error(
+                    'api: failed downloading image from "' + page + '" : ' + repr(err)
+                )
