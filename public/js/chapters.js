94ff2587579670b32b68cb2e63ea48ef0cea8e5d
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-05 16:42:56 +0100

    reformated

--- public/js/chapters.js
+++ public/js/chapters.js
@@ -4,23 +4,25 @@ const split_regex = /(\d[\d\-]*)(?:\/)*$/;
 const split_regex2 = /(\d[\d\-]*.*?)(?:\/)$/;
 
 function download_group(ichapters, manga, group) {
-  addNotification(".downloads-tab");
-  eel.download_group(ichapters, manga, group);
+	addNotification(".downloads-tab");
+	eel.download_group(ichapters, manga, group);
 }
-function download_chapter(chapter, manga) {
-  addNotification(".downloads-tab");
 
-  eel.download_group([chapter], open_manga, 1);
+function download_chapter(chapter, manga) {
+	addNotification(".downloads-tab");
+
+	eel.download_group([chapter], open_manga, 1);
 }
+
 function update_table(chapters_list) {
-  let table = $(".mdc-data-table__content");
-  table.parentNode.parentNode.mdc.destroy();
-  table.innerHTML = "";
-  chapters_list.forEach((elem) => {
-    let row = table.insertRow();
-    row.classList.add("mdc-data-table__row");
-    row.dataset["rowId"] = elem.number;
-    row.innerHTML = `
+	let table = $(".mdc-data-table__content");
+	table.parentNode.parentNode.mdc.destroy();
+	table.innerHTML = "";
+	chapters_list.forEach((elem) => {
+		let row = table.insertRow();
+		row.classList.add("mdc-data-table__row");
+		row.dataset["rowId"] = elem.number;
+		row.innerHTML = `
 			<td class="mdc-data-table__cell mdc-data-table__cell--checkbox">
 				<div class="mdc-checkbox mdc-data-table__row-checkbox">
 					<input
@@ -77,105 +79,114 @@ function update_table(chapters_list) {
 					</button>
 			</td>
 		`;
-  });
-  init.dataTable();
-  init.button();
+	});
+	init.dataTable();
+	init.button();
 }
+
 function update_chapters() {
-  let start = $("#start").value;
-  let end = $("#end").value;
-  let nlist = chapters;
-  if ((start && end && eval(start) < eval(end)) !== false) nlist = chapters.slice(eval(start) - 1, eval(end));
-  // console.log(nlist)
-  update_table(nlist);
+	let start = $("#start").value;
+	let end = $("#end").value;
+	let nlist = chapters;
+	nlist = nlist.sort((a,b) => {return a.number - b.number})
+	if ((start && end && eval(start) < eval(end)) !== false) {
+		nlist = chapters.slice(eval(start) - 1, eval(end));
+	}
+	nlist = nlist.sort((a,b) => {return b.number - a.number})
+	update_table(nlist);
 }
+
 function check_selected(e) {
-  let selected = e.target.mdc.getSelectedRowIds();
-  console.log(selected);
-  $(".download-selected").dataset.chapters = selected;
-  if (selected.length) {
-    $(".download-selected").removeAttribute("disabled");
-    $(".how-much-selected").style.display = "block";
-    $(".how-much-selected").innerHTML = selected.length + " selected";
-  } else {
-    $(".download-selected").setAttribute("disabled", "");
-    $(".how-much-selected").style.display = "none";
-  }
+	let selected = e.target.mdc.getSelectedRowIds();
+	console.log(selected);
+	$(".download-selected").dataset.chapters = selected;
+	if (selected.length) {
+		$(".download-selected").removeAttribute("disabled");
+		$(".how-much-selected").style.display = "block";
+		$(".how-much-selected").innerHTML = selected.length + " selected";
+	} else {
+		$(".download-selected").setAttribute("disabled", "");
+		$(".how-much-selected").style.display = "none";
+	}
 }
+
 async function load_infos(manga) {
-  $(".content").style.overflow = "hidden";
-  $(".load").style.opacity = 1;
+	let content = $(".content");
+	let load = $(".load");
+	content.style.overflow = "hidden";
+	load.style.opacity = 1;
 
-  const infos = await eel.get_info(manga.id)();
-  const favorites = await eel.get_favorites_id()();
+	const infos = await eel.get_info(manga.id)();
+	const favorites = await eel.get_favorites_id()();
 
-  // console.log(infos);
-  const b = $(".bookmark");
-  b.dataset.data = JSON.stringify(manga);
-  if (favorites && favorites.indexOf(manga.id) != -1) {
-    b.classList.add("bookmarked");
-    // b.querySelector(".mdc-button__label").innerHTML = "bookmarked"
-  } else {
-    b.classList.remove("bookmarked");
-  }
-  open_manga = manga.id;
+	const b = $(".bookmark");
+	b.dataset.data = JSON.stringify(manga);
+	if (favorites && favorites.indexOf(manga.id) !== -1) {
+		b.classList.add("bookmarked");
+	} else {
+		b.classList.remove("bookmarked");
+	}
+	open_manga = manga.id;
 
-  update_table(infos.chapters);
+	update_table(infos.chapters);
+	chapters = infos.chapters;
 
-  const details = $$("small.content");
-  details[0].innerHTML = infos.author;
-  details[1].innerHTML = infos.edition;
-  details[2].innerHTML = infos.release;
-  details[3].innerHTML = infos.status;
-  details[4].innerHTML = infos.alternatif;
-  details[5].innerHTML = infos.genres;
+	const details = $$("small.content");
+	details[0].innerHTML = infos.author;
+	details[1].innerHTML = infos.edition;
+	details[2].innerHTML = infos.release;
+	details[3].innerHTML = infos.status;
+	details[4].innerHTML = infos.alternatif;
+	details[5].innerHTML = infos.genres;
 
-  $(".title").innerHTML = infos.title;
-  $(".active_manga_review>.note").innerHTML = infos.note;
+	$(".title").innerHTML = infos.title;
+	$(".active_manga_review>.note").innerHTML = infos.note;
 
-  $(".manga-couverture").setAttribute("src", infos.cover);
-  $(".banner").style.backgroundImage = `url(${infos.banner})`;
-  $(".description").innerHTML = infos.description;
+	$(".manga-couverture").setAttribute("src", infos.cover);
+	$(".banner").style.backgroundImage = `url(${infos.banner})`;
+	$(".description").innerHTML = infos.description;
 
-  $(".load").style.opacity = 0;
-  $(".content").style.overflow = "initial";
+	load.style.opacity = 0;
+	content.style.overflow = "initial";
 
-  update_stars();
+	update_stars();
 }
+
 function addNotification(selector) {
-  let current = $(selector).style.getPropertyValue("--notif-text");
-  current = current ? parseInt(current.replaceAll('"', "")) + 1 : 1;
-  $(selector).style.setProperty("--notif-text", `"${current}"`);
+	let current = $(selector).style.getPropertyValue("--notif-text");
+	current = current ? parseInt(current.replaceAll('"', "")) + 1 : 1;
+	$(selector).style.setProperty("--notif-text", `"${current}"`);
 }
+
 function removeNotification(selector) {
-  let current = $(".bookmarks-tab").style.getPropertyValue("--notif-text");
-  current = current ? parseInt(current.replaceAll('"', "")) - 1 : 0;
-  if (current <= 0) {
-    $(".bookmarks-tab").style.removeProperty("--notif-text");
-  } else {
-    $(".bookmarks-tab").style.setProperty("--notif-text", `"${current}"`);
-  }
+	let current = $(".bookmarks-tab").style.getPropertyValue("--notif-text");
+	current = current ? parseInt(current.replaceAll('"', "")) - 1 : 0;
+	if (current <= 0) {
+		$(".bookmarks-tab").style.removeProperty("--notif-text");
+	} else {
+		$(".bookmarks-tab").style.setProperty("--notif-text", `"${current}"`);
+	}
 }
 
 $(".bookmark").onclick = async (e) => {
-  const favorites = await eel.get_favorites_id()();
-  const manga = JSON.parse($(".bookmark").dataset.data);
-  if (favorites && favorites.indexOf(manga.id) != -1) {
-    eel.remove_from_favorites(manga);
-    $(".bookmark").classList.remove("bookmarked");
-    removeNotification(".bookmarks-tab");
-  } else {
-    eel.add_to_favorites(manga);
-    $(".bookmark").classList.add("bookmarked");
-    addNotification(".bookmarks-tab");
-  }
-  // current = $(".bookmarks-tab").style.getPropertyValue("--notif-text")
-  // current = current ? parseInt(current.replaceAll('"', "")) : 0
-  // $(".bookmarks-tab").style.setProperty("--notif-text", `"${current + 1}"`)
+	const favorites = await eel.get_favorites_id()();
+	const manga = JSON.parse($(".bookmark").dataset.data);
+	if (favorites && favorites.indexOf(manga.id) != -1) {
+		eel.remove_from_favorites(manga);
+		$(".bookmark").classList.remove("bookmarked");
+		removeNotification(".bookmarks-tab");
+	} else {
+		eel.add_to_favorites(manga);
+		$(".bookmark").classList.add("bookmarked");
+		addNotification(".bookmarks-tab");
+	}
+	// current = $(".bookmarks-tab").style.getPropertyValue("--notif-text")
+	// current = current ? parseInt(current.replaceAll('"', "")) : 0
+	// $(".bookmarks-tab").style.setProperty("--notif-text", `"${current + 1}"`)
 };
 $(".download-selected").addEventListener("click", (e) => {
-  console.log(e.currentTarget.dataset.chapters.split(","), open_manga);
-  download_group(e.currentTarget.dataset.chapters.split(","), open_manga, $("#group").value);
+	console.log(e.currentTarget.dataset.chapters.split(","), open_manga);
+	download_group(e.currentTarget.dataset.chapters.split(","), open_manga, $("#group").value);
 });
 $("#start").onkeyup = update_chapters;
 $("#end").onkeyup = update_chapters;
--- public/js/connect.js
+++ public/js/connect.js
@@ -1,24 +1,23 @@
 const init = {
-	dataTable: _ => {
+	dataTable: (_) => {
 		$$(".mdc-data-table").forEach((e) => {
 			e.mdc = mdc.dataTable.MDCDataTable.attachTo(e);
 		});
 	},
 	ripple: (e) => {
-		if (e)
-			e.mdc = mdc.ripple.MDCRipple.attachTo(e);
+		if (e) e.mdc = mdc.ripple.MDCRipple.attachTo(e);
 	},
-	textField: _ => {
+	textField: (_) => {
 		$$(".mdc-text-field").forEach((e) => {
 			e.mdc = mdc.textField.MDCTextField.attachTo(e);
 		});
 	},
-	button: _ => {
+	button: (_) => {
 		$$(".mdc-button").forEach((e) => {
 			e.mdc = mdc.ripple.MDCRipple.attachTo(e);
 		});
 	},
-	linearProgress: _ => {
+	linearProgress: (_) => {
 		$$(".mdc-linear-progress").forEach((e) => {
 			e.mdc = mdc.linearProgress.MDCLinearProgress.attachTo(e);
 		});
@@ -26,21 +25,20 @@ const init = {
 			e.mdc.determinate = false;
 		});
 	},
-	select: _ => {
-
+	select: (_) => {
 		$$(".mdc-select").forEach((e) => {
 			if (e.mdc) e.mdc.destroy();
 			e.mdc = mdc.select.MDCSelect.attachTo(e);
 		});
-	}
+	},
 };
 
 function init_all() {
 	for (const type in init) {
-		init[type]()
+		init[type]();
 	}
 }
 
 // window.mdc.autoInit();
 // update_chapters();
-init_all()
+init_all();
